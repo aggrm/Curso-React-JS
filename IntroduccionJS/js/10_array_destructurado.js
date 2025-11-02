3c58b0586b2c15d165473af7b2649db1247c83b9
Alberto Goujon <alberto.goujon@gmail.com>
2025-11-02 22:56:09 +0100

    hasta clase 22

---
+++ IntroduccionJS/js/10_array_destructurado.js
@@ -0,0 +1,11 @@
+const tecnologias = ['HTML', 'CSS', 'JavaScript', 'React.js', 'Node.js']
+
+const react = tecnologias[3]
+
+//Destructuring arrays
+
+//const [html, css, js, reactjs] = tecnologias
+
+const [ , , , reactjs] = tecnologias
+
+console.log(reactjs)
