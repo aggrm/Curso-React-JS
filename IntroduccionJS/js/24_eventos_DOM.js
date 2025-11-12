3575d57d680ac2c3ee56bd645975b5a576396b5b
Alberto Goujon <alberto.goujon@gmail.com>
2025-11-12 18:38:12 +0100

    Seccion 1 de 71 terminada

--- IntroduccionJS/js/24_eventos_DOM.js
+++ IntroduccionJS/js/24_eventos_DOM.js
@@ -16,3 +16,61 @@ enlaces.forEach(enlace => {
         e.target.textContent = 'Diste click'
     })
 })
+
+//======================================================================================================================================
+// Inputs
+//======================================================================================================================================
+
+const inputNombre = document.querySelector('#nombre')
+
+inputNombre.addEventListener('input', (e) =>{
+    console.log(e.target.value);
+})
+
+const inputPassword = document.querySelector('#password')
+inputPassword.addEventListener('input', pwd)
+function pwd(){
+    inputPassword.type="text"
+
+    setTimeout(() => {
+        inputPassword.type='password'
+    }, 300)
+
+}
+
+//======================================================================================================================================
+// Submit
+//======================================================================================================================================
+const formuario = document.querySelector('#formulario')
+formuario.addEventListener('submit', e =>{
+
+    e.preventDefault() //quitar la accion por defecto del evento o selector dom
+    const nombre = document.querySelector('#nombre').value
+    const password = document.querySelector('#password').value
+    
+
+    //Prevenir nuevas alertas
+    const alertaPrevia = document.querySelector('.alerta')
+    alertaPrevia?.remove()
+    
+
+    const alerta = document.querySelector('DIV')
+    alerta.classList.add('alerta', 'text-white', 'uppercase', 'text-sm', 'text-center', 'p-2', 'font-black')
+    
+
+    if(nombre  === '' || password === ''){
+        alerta.textContent = 'Todos los campos son obligatorios...'
+        alerta.classList.add('bg-red-500')
+    }else{
+        alerta.textContent = 'Todos los campos son CORRECTOS'
+        alerta.classList.add('bg-green-500')
+    }
+    console.log(alerta);
+    
+    formuario.appendChild(alerta)
+    setTimeout(() =>{
+        alerta.remove()
+    }, 3000);
+
+    console.log('Enviando formulario...');
+})
